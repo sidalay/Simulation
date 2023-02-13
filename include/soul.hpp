d28ea4f9f98d05e47c90410a6d025be24c1a0219
Sid Shingaki <sidalaytv@gmail.com>
2023-02-13 04:46:03 -0800

    chore: renamed bitmasks

--- include/soul.hpp
+++ include/soul.hpp
@@ -4,44 +4,50 @@
 #include "raylib.h"
 #include <bitset>
 #include <random>
+#include <array>
 
 #define NEUTRAL       0b0000'0000
-#define HAPPY         0b0000'0001
-#define SAD           0b0000'0010
-#define ANGRY         0b0000'0100
-#define LOVE          0b0000'1000
+#define FRIENDLY      0b0000'0001
+#define HOSTILE       0b0000'0010
+#define CURIOUS       0b0000'0100
+#define ENLIGHTENED   0b0000'1000
 #define CONFUSED      0b0001'0000
-#define ENLIGHTENED   0b0010'0000
-#define ANXIOUS       0b0100'0000
-#define APATHETIC     0b1000'0000
+#define ANXIOUS       0b0010'0000
+#define FRIGHTENED    0b0100'0000
+#define INSANITY      0b1000'0000
 
 class Soul {
 public:
   void Tick();
   void Draw();
-  [[nodiscard]] const Rectangle GetPos() const {return m_body;}
+  [[nodiscard]] const Rectangle GetRectangle() const {return m_body;}
   [[nodiscard]] const Color GetColor() const {return m_color;}
+  [[nodiscard]] const int GetId() const {return m_id;}
 
   // debug
   // ---------------------- //
   void DrawSpatial();
   // ---------------------- //
 public:
-  Soul(Rectangle body, 
+  Soul(int id,
+       Rectangle body, 
        Color color, 
        Vector2i& window, 
-       std::mt19937_64& rng, 
+       std::mt19937_64& rng,
+       std::array<Soul, 7>& souls, 
        std::bitset<8> emotion = NEUTRAL);
 
 private:
   Rectangle       m_body{};
   Color           m_color{};
+  int             m_id;
   int             m_speed{10};
   float           m_deltaTime{};
   float           m_updateTime{5.f};
   std::bitset<8>  m_emotions{NEUTRAL};
-  Vector2i&       m_window;
 
+  Vector2i&                           m_window;
+  std::array<Soul, 7>&                m_souls;
   std::mt19937_64&                    m_rng;
   std::uniform_int_distribution<int>  m_direction{0, 3};
 
@@ -49,12 +55,12 @@ private:
   // ---------------------- //
   Rectangle       m_spatialCollision{};
   Color           m_spatialColor{WHITE};
-  bool decrease{};
+  bool            m_decrease{};
   // ---------------------- //
 private:
   void UpdatePos();
   void CheckOutOfBounds();
-  [[nodiscard]] const bool Colliding() const;
+  [[nodiscard]] const bool CheckColliding() const;
 
   // debug
   // ---------------------- //
