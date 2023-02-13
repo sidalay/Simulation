1afc3aab7cf99786dfc49d42ac05583f74e49d04
Sid Shingaki <sidalaytv@gmail.com>
2023-02-12 20:43:29 -0800

    chore: move soul declarations into an array

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 #include "soul.hpp"
 
 int main(void)
@@ -13,22 +14,33 @@ int main(void)
   std::random_device seed;
   std::mt19937_64 rand{seed()};
 
-  Soul sOne{Rectangle{290, 290, 10, 10}, GREEN, window, rand};
-  Soul sTwo{Rectangle{310, 310, 10, 10}, BLUE, window, rand};
+  std::array<Soul, 7> souls{
+    {
+      {Rectangle{290, 290, 10, 10}, GREEN, window, rand},
+      {Rectangle{370, 310, 10, 10}, BLUE, window, rand},
+      {Rectangle{480, 480, 10, 10}, RED, window, rand},
+      {Rectangle{250, 50, 10, 10}, ORANGE, window, rand},
+      {Rectangle{30, 500, 10, 10}, YELLOW, window, rand},
+      {Rectangle{100, 100, 10, 10}, PURPLE, window, rand},
+      {Rectangle{500, 150, 10, 10}, WHITE, window, rand}
+    }
+  };
 
   // Main game loop
   while (!WindowShouldClose())
   {
     // Update
-    sOne.Tick();
-    sTwo.Tick();
+    for (auto& soul:souls) {
+      soul.Tick();
+    }
 
     // Draw
     BeginDrawing();
 
     ClearBackground(BLACK);
-    sOne.Draw();
-    sTwo.Draw();
+    for (auto& soul:souls) {
+      soul.Draw();
+    }
 
     EndDrawing();
   }
