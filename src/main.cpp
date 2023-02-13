e3f81a42820ae47d0590f59b007bd5c7876bd528
Sid Shingaki <sidalaytv@gmail.com>
2023-02-12 20:24:06 -0800

    chore: update to include soul.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,38 @@
-#include "raylib.h"
 #include <iostream>
-#include <random>
+#include "soul.hpp"
 
 int main(void)
 {
-    // Initialization
-    const int screenWidth = 600;
-    const int screenHeight = 600;
+  // Initialization
+  Vector2i window{600, 600};
 
-    InitWindow(screenWidth, screenHeight, "Simulation");
-    SetTraceLogLevel(LOG_NONE);
-    SetTargetFPS(144);
+  InitWindow(window.x, window.y, "Simulation");
+  SetTraceLogLevel(LOG_NONE);
+  SetTargetFPS(144);
 
-    int height{10};
-    int width{10};
-    int moveSpeed{5};
-    float deltaTime{};
-    float runningTime{};
-    float updateTime{5.f};
+  std::random_device seed;
+  std::mt19937_64 rand{seed()};
 
-    std::random_device seed;
-    std::mt19937_64 rand{seed()};
+  Soul sOne{Rectangle{290, 290, 10, 10}, GREEN, window, rand};
+  Soul sTwo{Rectangle{310, 310, 10, 10}, BLUE, window, rand};
 
-    Rectangle sOne{290, 290, 10, 10};
-    Rectangle sTwo{310, 310, 10, 10};
+  // Main game loop
+  while (!WindowShouldClose())
+  {
+    // Update
+    sOne.Tick();
+    sTwo.Tick();
 
-    // Main game loop
-    while (!WindowShouldClose())
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        std::uniform_int_distribution<int> sOneId(0, 3);
-        std::uniform_int_distribution<int> sTwoId(0, 3);
+    // Draw
+    BeginDrawing();
 
-        runningTime += 0.1;
+    ClearBackground(BLACK);
+    sOne.Draw();
+    sTwo.Draw();
 
-        if (runningTime >= updateTime) {
-            switch (sOneId(rand)) {
-                case 0:
-                    sOne.x -= moveSpeed;
-                    break;
-                case 1:
-                    sOne.x += moveSpeed;
-                    break;
-                case 2:
-                    sOne.y -= moveSpeed;
-                    break;
-                case 3:
-                    sOne.y += moveSpeed;
-                    break;
-            }
-            switch (sTwoId(rand)) {
-                case 0:
-                    sTwo.x -= moveSpeed;
-                    break;
-                case 1:
-                    sTwo.x += moveSpeed;
-                    break;
-                case 2:
-                    sTwo.y -= moveSpeed;
-                    break;
-                case 3:
-                    sTwo.y += moveSpeed;
-                    break;
-            }
-            runningTime = 0;
-        }
+    EndDrawing();
+  }
 
-        if (sOne.x < 0) {
-            sOne.x = 0;
-        }
-        if (sOne.y < 0) {
-            sOne.y = 0;
-        }
-        if (sOne.x > screenWidth - width) {
-            sOne.x = screenWidth - width;
-        }
-        if (sOne.y > screenHeight - height) {
-            sOne.y = screenHeight - height;
-        }
-        
-        if (sTwo.x < 0) {
-            sTwo.x = 0;
-        }
-        if (sTwo.y < 0) {
-            sTwo.y = 0;
-        }
-        if (sTwo.x > screenWidth - width) {
-            sTwo.x = screenWidth - width;
-        }
-        if (sTwo.y > screenHeight - height) {
-            sTwo.y = screenHeight - height;
-        }
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        BeginDrawing();
-
-            ClearBackground(BLACK);
-            DrawRectangle(sOne.x, sOne.y, width, height, GREEN);
-            DrawRectangle(sTwo.x, sTwo.y, width, height, BLUE);
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+  CloseWindow();
+  return 0;
 }
