8ca5349cad1a4cb2a3963c0e27074aec00df5708
Sid Shingaki <sidalaytv@gmail.com>
2023-02-13 23:36:45 -0800

    chore: create world space vs window space

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main(void)
 {
   // Initialization
   Vector2i window{600, 600};
+  Vector2i world{600, 400};
 
   InitWindow(window.x, window.y, "Simulation");
   SetTraceLogLevel(LOG_NONE);
@@ -17,13 +18,13 @@ int main(void)
 
   std::array<Soul, 7> souls{
     {
-      {1, Rectangle{290, 290, 10, 10}, GREEN, window, rand, souls},
-      {2, Rectangle{370, 310, 10, 10}, BLUE, window, rand, souls},
-      {3, Rectangle{480, 480, 10, 10}, RED, window, rand, souls},
-      {4, Rectangle{250, 50, 10, 10}, ORANGE, window, rand, souls},
-      {5, Rectangle{30, 500, 10, 10}, YELLOW, window, rand, souls},
-      {6, Rectangle{100, 100, 10, 10}, PURPLE, window, rand, souls},
-      {7, Rectangle{500, 150, 10, 10}, WHITE, window, rand, souls}
+      {1, Rectangle{290, 290, 10, 10}, GREEN, world, rand, souls},
+      {2, Rectangle{370, 310, 10, 10}, BLUE, world, rand, souls},
+      {3, Rectangle{380, 380, 10, 10}, RED, world, rand, souls},
+      {4, Rectangle{250, 50, 10, 10}, ORANGE, world, rand, souls},
+      {5, Rectangle{30, 400, 10, 10}, YELLOW, world, rand, souls},
+      {6, Rectangle{100, 100, 10, 10}, PURPLE, world, rand, souls},
+      {7, Rectangle{500, 150, 10, 10}, WHITE, world, rand, souls}
     }
   };
 
